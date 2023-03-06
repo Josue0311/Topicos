fffe6ee9b7350065db03214969145d0aecafbd18
KevinYam <99611273+KevinYam22@users.noreply.github.com>
2023-03-05 20:45:38 -0600

    LIstoooooooo

---
+++ Eats.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import java.applet.Applet;
+public class  Eats extends JFrame {
+    public Eats(){
+        this.setSize(1280,720);
+        this.setTitle("Estudents Eats");
+        this.setVisible(true);
+    }
+   
+    public static void main(String args[]){
+        Eats eas = new Eats();
+    }
+}
