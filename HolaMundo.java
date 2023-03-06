1cf878bb116a587d022bec43f9d3b97d6bea38ce
Josue0311 <99627950+Josue0311@users.noreply.github.com>
2023-03-05 20:43:09 -0600

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,36 @@
+public class HolaMundo{
+    public static void main(String[] args) {
+		double numero = 7.0;
+		//System.out.printf("El número originalmente es: %f\n", numero);
+		double parteDecimal = numero % 8; // Lo que sobra de dividir al número entre 1
+		//double parteEntera = numero - parteDecimal; // Le quitamos la parte decimal usando una resta
+		System.out.println(parteDecimal);
+	}
+
+}
+//import java.util.Scanner;
+//public class NumAleatorio {
+  //  public static void main (String args []){
+    //    Scanner sc = new Scanner(System.in);
+      //  System.out.println("a=multiplicador \n c=constante aditiva \n m=modulo \n x= la semilla");
+        //System.out.println("Cuantos numeros pseudoaleatorios necesita: ");
+        //int num=sc.nextInt();
+       //double a, c, m, x;
+        //System.out.println("Escribe el valor de a: ");
+        //a=sc.nextDouble();
+        //System.out.println("Escribe el valor de c: ");
+        //c=sc.nextDouble();
+        //System.out.println("Escribe el valor de m: ");
+        //m=sc.nextDouble();
+        //System.out.println("Escribe el valor de x: ");
+        //x=sc.nextDouble();
+        //for(int i=1; i<=num;i++){
+        //    double f=(a*x)+c;
+         //   double r1=f%m;
+          //  double r=r1/m;
+           // System.out.println("Numero pseudoaleatorio " + i + ": " + r);
+        //    x=r1;            
+        //}
+        
+    //}
+//}
